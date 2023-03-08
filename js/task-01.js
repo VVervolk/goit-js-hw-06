ac309b0e2baa6568c86d20fad7fbf852f4673733
VVervolk <vervolk1234@gmail.com>
2023-03-08 13:51:39 +0200

    2 task complete

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const item = document.querySelectorAll(".item");
 
 function onCategoriesQuantity(array) {
-  console.log(`Number of categories: ${item.length}`),
+  console.log(`Number of categories: ${array.length}`),
     array.forEach(function (category) {
       console.log(`Category: ${category.firstElementChild.textContent} `);
       console.log(`Elements: ${category.lastElementChild.children.length}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+function onCreateIngredient(array) {
+  const arrayOfItems = [];
+  array.forEach((ingredient) => {
+    const item = document.createElement("li");
+    item.textContent = ingredient;
+    item.classList.add("item");
+    arrayOfItems.push(item);
+  });
+  ingredientsList.append(...arrayOfItems);
+}
+onCreateIngredient(ingredients);
