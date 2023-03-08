4bdfb35e542d39e6a7d6ab6a83db5f63236559f4
VVervolk <vervolk1234@gmail.com>
2023-03-08 10:14:27 +0200

    1 task complete

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const item = document.querySelectorAll(".item");
+
+function onCategoriesQuantity(array) {
+  console.log(`Number of categories: ${item.length}`),
+    array.forEach(function (category) {
+      console.log(`Category: ${category.firstElementChild.textContent} `);
+      console.log(`Elements: ${category.lastElementChild.children.length}`);
+    });
+  return;
+}
+
+onCategoriesQuantity(item);
