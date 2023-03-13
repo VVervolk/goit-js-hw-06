67ab696e45c1c2dee2ec115ad8821e8d4b6ffe26
VVervolk <vervolk1234@gmail.com>
2023-03-13 17:02:07 +0200

    complete 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const boxes = document.querySelector("#boxes");
+const quantity = document.querySelector("input[type = 'number']");
+const buttonCreate = document.querySelector("button[data-create]");
+const buttonDestroy = document.querySelector("button[data-destroy]");
+
+function createBoxes(amount) {
+  const elements = [];
+  let widthForLoop = 30;
+  let heightForLoop = 30;
+
+  for (
+    let index = 1;
+    index <= amount;
+    index += 1, widthForLoop += 10, heightForLoop += 10
+  ) {
+    const div = document.createElement("div");
+    div.style.width = `${widthForLoop}px`;
+    div.style.height = `${heightForLoop}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    elements.push(div);
+  }
+
+  boxes.append(...elements);
+}
+
+buttonCreate.addEventListener("click", () => createBoxes(quantity.value));
+
+function destroyBoxes() {
+  boxes.replaceChildren();
+}
+
+buttonDestroy.addEventListener("click", () => destroyBoxes());
