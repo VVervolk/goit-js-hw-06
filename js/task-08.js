ef844484de47930056944b5b1445b6e4d404c3e8
VVervolk <vervolk1234@gmail.com>
2023-03-13 15:08:06 +0200

    8 task complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = form;
+
+  if (!email.value || !password.value) {
+    alert("Please fill in all the fields!");
+  }
+  console.log({ email: email.value, password: password.value });
+  event.currentTarget.reset();
+});
