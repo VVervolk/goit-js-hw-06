a877d0099047e736f5da1b5d073ee964f19674b5
VVervolk <vervolk1234@gmail.com>
2023-03-13 15:25:45 +0200

    task 9 complete

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const buttonForChangeColor = document.querySelector(".change-color");
+const colorValue = document.querySelector(".color");
+
+buttonForChangeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
